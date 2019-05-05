97f122389b75215b3a40e903360b9471ba509a77
knail1 <oansari@gmail.com>
2019-05-04 21:39:45 -0500

    fixed routes

--- app.py
+++ app.py
@@ -38,14 +38,34 @@ def pdfParser(pdfFile):
 
 
 
-@app.route('/asr')
+def selectURI(x):
+    return {
+        'January': 'http://masjidyaseen.org/wp-content/uploads/2019/04/January-2020.pdf',
+        'Febuary': 'http://masjidyaseen.org/wp-content/uploads/2019/04/Febuary-2020.pdf',
+        'March': 'http://masjidyaseen.org/wp-content/uploads/2019/04/March-2020.pdf',
+        'April': 'http://masjidyaseen.org/wp-content/uploads/2019/04/April-2019.pdf',
+        'May': 'http://masjidyaseen.org/wp-content/uploads/2019/05/May-2019.pdf',
+        'June': 'http://masjidyaseen.org/wp-content/uploads/2019/05/June-2019.pdf',
+        'July': 'http://masjidyaseen.org/wp-content/uploads/2019/05/July-2019.pdf',
+        'August': 'http://masjidyaseen.org/wp-content/uploads/2019/05/August-2019.pdf',
+        'September': 'http://masjidyaseen.org/wp-content/uploads/2019/05/September-2019.pdf',
+        'October': 'http://masjidyaseen.org/wp-content/uploads/2019/05/October-2019.pdf',
+        'November': 'http://masjidyaseen.org/wp-content/uploads/2019/05/November-2019.pdf',
+        'December': 'http://masjidyaseen.org/wp-content/uploads/2019/05/December-2019.pdf',
+
+        }[x]
+
+@app.route('/')
 def todaysPrayerTimes():
-    aprilUri = 'http://masjidyaseen.org/wp-content/uploads/2019/04/April-2019.pdf'
-    pdfFileName = pullPdf(aprilUri)
+    currentDT = datetime.datetime.now()
+
+    #aprilUri = 'http://masjidyaseen.org/wp-content/uploads/2019/04/April-2019.pdf'
+    #mayUri = 'http://masjidyaseen.org/wp-content/uploads/2019/05/May-2019.pdf'
+    theUri = selectURI(currentDT.strftime("%B"))
+    pdfFileName = pullPdf(theUri)
     df_output = pdfParser(pdfFileName)
     print(df_output)
 
-    currentDT = datetime.datetime.now()
     currentDay = int(currentDT.strftime("%d"))
 
     if df_output.columns[1] == currentDT.strftime("%B"):
@@ -55,7 +75,7 @@ def todaysPrayerTimes():
     return render_template('index.html', title='PrayerSched', asrTiming = asrTiming, table=df_output.to_html(index=False))
 
 
-@app.route('/')
+@app.route('/upload')
 def upload():
     return render_template('upload.html')
 
@@ -64,8 +84,10 @@ def upload():
 def uploader():
     if request.method == 'POST':
         f = request.files['file']
+        # os.path.join(app.config['UPLOAD_FOLDER'], filename)
         f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+        #return 'file uploaded successfully'
+        return render_template('postUpload.html', fileName = f.filename)
 
 
 
